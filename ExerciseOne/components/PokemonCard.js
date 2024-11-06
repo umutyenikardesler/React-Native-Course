fd607324d32f1d554c3df5bf45d34c5ee2998ba6
umutyenikardesler <your_email_2@example.com>
2024-11-06 16:06:42 +0300

    continue for 46

--- ExerciseOne/components/PokemonCard.js
+++ ExerciseOne/components/PokemonCard.js
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function PokemonCard() {
     return (
         <View>
-            <Text>Pokemon Card</Text>
+            <Text style={styles.card}>Pokemon Card</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 8,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowOffset: { width: 2, height: 2},
+                shadowColor: "#333",
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+            },
+            android: {
+                elevation: 5,
+                color: "#000",
+            },
+        })
+    }
+    });
