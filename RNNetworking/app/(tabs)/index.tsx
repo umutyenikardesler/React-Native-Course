e01b00be49787ec8f0e4249217777222e7d3f96f
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-19 17:29:46 +0300

    Upd: 67 - GET Requests

--- RNNetworking/app/(tabs)/index.tsx
+++ RNNetworking/app/(tabs)/index.tsx
@@ -1,74 +1,60 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { SafeAreaView, StatusBar, View,  Text, StyleSheet, FlatList} from 'react-native';
+import { useState, useEffect } from 'react';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function App() {
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+const [postList, setPostList] = useState([])
+
+const fetchData = async (limit = 10) => {
+  const response = await fetch (
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
   );
-}
+  const data = await response.json()
+  setPostList(data)
+};
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+  return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.listContainer}>
+      <FlatList data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.title} </Text>
+                <Text style={styles.bodyText}>{item.body} </Text>
+              </View>
+            )
+          }}
+          />
+    </View>
+  </SafeAreaView>
+)};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  listContainer: {
+    flex: 1,
+    paddingHorizontal:16,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize:30,
+  },
+  bodyText: {
+    fontSize: 24,
+    color: "#666666",
   },
 });
