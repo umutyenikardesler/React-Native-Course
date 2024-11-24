d28f8eb3a8b3feb02dc7a5e701416b83a4b4e21b
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-25 00:17:54 +0300

    Upd: 73 - Stack Navigation

--- RNNavigation/app/(tabs)/index.tsx
+++ RNNavigation/app/(tabs)/index.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "@/screens/HomeScreen";
+import AboutScreen from "@/screens/AboutScreen";
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='About'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
