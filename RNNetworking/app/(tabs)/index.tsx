f760a182ab8c1387796f302eec2c1aa10be90b6c
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-24 22:09:02 +0300

    Upd: 69 - Pull to Refresh

--- RNNetworking/app/(tabs)/index.tsx
+++ RNNetworking/app/(tabs)/index.tsx
@@ -1,21 +1,40 @@
-import { SafeAreaView, StatusBar, View,  Text, StyleSheet, FlatList} from 'react-native';
+import { SafeAreaView, StatusBar, View, Text, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 import { useState, useEffect } from 'react';
 
 export default function App() {
 
-const [postList, setPostList] = useState([])
+const [postList, setPostList] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+
+const [refrehing, setRefreshing] = useState(false);
 
 const fetchData = async (limit = 10) => {
-  const response = await fetch (
+  const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
   );
-  const data = await response.json()
-  setPostList(data)
+  const data = await response.json();
+  setPostList(data);
+  setIsLoading(false);
 };
+
+const handleRefresh = () => {
+  setRefreshing(true)
+  fetchData(20)
+  setRefreshing(false)
+}
 
 useEffect(() => {
   fetchData();
 }, []);
+
+if(isLoading) {
+  return (
+    <SafeAreaView style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color="0000ff" />
+      <Text>Loading...</Text>
+    </SafeAreaView>
+  )
+}
 
   return (
   <SafeAreaView style={styles.container}>
@@ -27,8 +46,20 @@ useEffect(() => {
                 <Text style={styles.titleText}>{item.title} </Text>
                 <Text style={styles.bodyText}>{item.body} </Text>
               </View>
-            )
+            );
           }}
+          ItemSeparatorComponent={() => (
+            <View style={{
+              height: 16,
+            }} /> 
+          )}
+          ListEmptyComponent={ <Text> No Posts Found </Text> }
+          ListHeaderComponent={ <Text style={styles.headerText}> Post List </Text> }
+          ListFooterComponent={ <Text style={styles.footerText}> End of List </Text> }
+
+          refreshing={refrehing}
+          onRefresh={handleRefresh}
+
           />
     </View>
   </SafeAreaView>
@@ -57,4 +88,23 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "#666666",
   },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+    marginTop: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+    marginTop: 12,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
