be8abdcead893f3cbac9ec2b1fdeb325da540eeb
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-26 15:40:17 +0300

    Upd: 73 - Stack Navigation

--- RNNavigation/app/(tabs)/index.tsx
+++ RNNavigation/app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "@/screens/HomeScreen";
-import AboutScreen from "@/screens/AboutScreen";
+import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '@/screens/HomeScreen';
+import AboutScreen from '@/screens/AboutScreen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='About'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="About">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
