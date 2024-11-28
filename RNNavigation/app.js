5fd9c5ae5f208a62096129e74f870f10abd338a0
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-28 17:14:04 +0300

    Upd: 81 - Tab Navigation Options

--- RNNavigation/app.js
+++ RNNavigation/app.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CourseListScreen from '@/screens/CourseListScreen';
 import ProfileScreen from '@/screens/ProfileScreen';
 import SettingsScreen from '@/screens/SettingsScreen';
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,9 +11,28 @@ export default function App() {
     return(
         <NavigationIndependentTree>
             <NavigationContainer>
-                <Tab.Navigator>
-                    <Tab.Screen name='Course List' component={CourseListScreen} />
-                    <Tab.Screen name='Profile' component={ProfileScreen} />
+                <Tab.Navigator screenOptions={{
+                    tabBarLabelPosition: "below-icon",
+                    tabBarShowLabel: true,
+                    tabBarActiveTintColor: "purple",
+                }}>
+                    <Tab.Screen 
+                        name='Course List' 
+                        component={CourseListScreen} 
+                        options={{
+                            tabBarLabel: "My CourseList",
+                            tabBarIcon: () => <Ionicons name={"person"} size={20} />,
+                            tabBarBadge: 3,
+                        }} />
+                    <Tab.Screen
+                        name="Profile"
+                        component={ProfileScreen}
+                        options={{
+                            tabBarLabel: "My Profile",
+                            tabBarIcon: () => <Ionicons name={"person"} size={20} />,
+                            tabBarBadge: 3,
+                        }}
+                        />
                     <Tab.Screen name='Settings' component={SettingsScreen} />
                 </Tab.Navigator>
             </NavigationContainer>
