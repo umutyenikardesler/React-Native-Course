cf24b9a8dbcafb073724457a0ccb05ef51a7e539
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-11-30 22:56:19 +0300

    Tailwind Modifed

--- NativeWindApps/app/_layout.jsx
+++ NativeWindApps/app/_layout.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { View, Text} from 'react-native';
 import { Stack } from "expo-router";
 import "../global.css";
 
 const RootLayout = () =>  {
   return (
     <Stack>
-        <Stack Screen name="index" />
+        <Stack.Screen name="index" />
     </Stack>
   );
 }
